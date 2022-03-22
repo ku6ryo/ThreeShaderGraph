ed4acad07d14655bc9191b27e6de8697ecdecb2f
ku6ryo <ku6ryo@gmail.com>
2022-03-22 18:50:16 +0900

    removed unused types

--- src/definitions/types.ts
+++ src/definitions/types.ts
@@ -1,17 +1,3 @@
-export enum NodeDataType {
-  Float = 0,
-  Vector2 = 1,
-  Vector3 = 2,
-  Vector4 = 3,
-}
-
-export const mathTypes = [
-  NodeDataType.Float,
-  NodeDataType.Vector2,
-  NodeDataType.Vector3,
-  NodeDataType.Vector4,
-]
-
 export type NodeCategory = {
   id: string,
   label: string,
